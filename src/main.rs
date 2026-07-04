2fedbbe3e12115b3b10b894fc9b224e8f6fcf26e
chumeng <2595248810@qq.com>
2026-07-04 10:13:28 +0800

    First commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    const WIDTH: u32 = 256;
+    const HEIGHT: u32 = 256;
+    
+    println!("P3\n{} {}\n255", WIDTH, HEIGHT);
+    for j in 0..HEIGHT {
+        for i in 0..WIDTH {
+            let r = (i as f64) / ((WIDTH as f64) - 1.0);
+            let g = (j as f64) / ((HEIGHT as f64) - 1.0);
+            let b = 0.0;
+            let r = (255.999 * r) as i32;
+            let g = (255.999 * g) as i32;
+            let b = (255.999 * b) as i32;
+            println!("{} {} {}", r, g, b);
+        }
+    }
+}
